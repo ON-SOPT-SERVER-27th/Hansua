2dc8acdd226fce3f7b89a15e3cf6938f0ea0cba6
sssua-0928 <72644361+sssua-0928@users.noreply.github.com>
2020-10-16 15:56:42 +0900

    1_level2

---
+++ 1주차/1stLevel2.js
@@ -0,0 +1,16 @@
+//2. json array
+const teamMember = [
+    {
+        "name": "한수아"
+        "part": "Server"
+        "status" : "YB"
+    }
+];
+
+
+const getMemberList = teamMember.filter((m) => m.name === "한수아")
+(ers) = {
+    members.filter(member => {
+        console 
+    })
+}
